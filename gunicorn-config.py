361306ae200cecce21138892f7f240a386d15730
SMG-Docker-01 <silva.brian.987@gmail.com>
2018-11-27 16:49:19 -0300

    /home/devops commit in process 8002

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -1,1 +1,19 @@
 bind = "0.0.0.0:8080"
+log-config-dict = {
+	"handlers" : [
+		"console" : {
+			"class" : "logging.StreamHandler",
+			"formatter" : "brief",
+			"level" : "INFO",
+			"filters" : "[allow_foo]",
+			"stream" : "ext://sys.stdout"
+		},
+		"file" : {
+			"class" : "logging.handlers.RotatingFileHandler",
+			"formatter" : "precise",
+			"filename" : "logconfig.log",
+			"maxBytes" : "1024",
+			"backupCount" : "3"
+		}
+	]
+}
